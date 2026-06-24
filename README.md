6f265aacc862a5f7b4abf1dd8a909f7843e38745
actionchain <actionchain@chainguard.dev>
2026-06-24 02:33:10 +0000

    Update chalk-ai/cli-action@v3.2

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# chalk-ai-cli-action
+# `cli-action`
+> Sets up the Chalk CLI for tests in GitHub
+
+## About
+This action sets up the Chalk CLI, `chalk` on GitHub's hosted Actions runners.
+
+This action can be run on `ubuntu-latest`, `windows-latest`, and `macos-latest` 
+GitHub Actions runners, and will install and expose a specified version of the 
+`chalk` CLI on the runner environment.
+
+## Deploying to a branch
+
+Set up the `chalk` CLI and use it to deploy to a branch:
+
+```yaml
+steps:
+- uses: chalk-ai/cli-action@v1
+  with:
+    client-id: token-dlkanvoinohnaon4oindoivn
+    client-secret: ts-5901039nlbm3n537el367b9025dc305
+
+- name: Deploy to branch
+  run: chalk apply --branch ${{ github.head_ref || github.ref_name }}
+```
+
+A specific version of the `chalk` CLI can be installed:
+
+```yaml
+steps:
+- uses: chalk-ai/cli-action@v1
+  with:
+    version: 1.1.0
+    client-id: token-dlkanvoinohnaon4oindoivn
+    client-secret: ts-5901039nlbm3n537el367b9025dc305
+
+- name: Deploy to branch
+  run: chalk apply --branch ${{ github.head_ref || github.ref_name }}
+```
+
+## Inputs
+The action supports the following inputs:
+
+- `client-id`: The Chalk Client ID from the tokens page in your settings.
+- `client-secret`: The Chalk Client Secret from the tokens page in your settings.
+- `version` (optional): The version of `chalk` to install, defaulting to `latest`.
+- `api-host` (optional): If you're using a self-hosted deployment, the API host where Chalk is hosted.
+- `environment` (optional): The Chalk environment to use. Your token is typically scoped to a single environment, and you won't need to use this parameter.
+
+## License
+Apache 2.0
+
+## Privacy
+
+This Action contacts Chainguard's licensing server to verify authorization. Connection metadata (IP address, GitHub repository identifier, timestamp, and any metadata encoded in the auth token) is transmitted to Chainguard, Inc. even if authorization is denied in accordance with our [Privacy Notice](https://www.chainguard.dev/legal/privacy-notice)
---
+++ src/main.ts
@@ -0,0 +1,69 @@
+import * as core from '@actions/core'
+import * as http from '@actions/http-client'
+import * as toolCache from '@actions/tool-cache'
+import {promises} from 'fs'
+import * as fs from 'fs';
+import * as os from 'os'
+import { exec } from 'child_process'
+import { promisify } from 'util'
+const execAsync = promisify(exec)
+
+
+const client = new http.HttpClient('setup-chalk-cli')
+const TOOL_NAME = 'chalk'
+const home = os.homedir()
+
+async function run() {
+  // Get user-specified version to install (defaults to "latest")
+  const suppliedVersion = core.getInput('version')
+  const os = process.platform
+  const arch = process.arch
+  const version = suppliedVersion == 'latest' ? await getLatestVersion(os, arch) : suppliedVersion
+  const dir = `${home}/.config`
+
+  const cachedPath = toolCache.find(TOOL_NAME, version)
+  if (cachedPath && version != 'nightly') {
+    core.addPath(cachedPath)
+  } else {
+    core.info(`Downloading version ${version}`)
+    const path = await toolCache.downloadTool(`https://api.prod.chalk.ai/v1/install/${os}/${arch}/${version}`)
+    fs.chmodSync(path, 0o755)
+    const cachedPath = await toolCache.cacheFile(path, TOOL_NAME, TOOL_NAME, version, undefined)
+    fs.chmodSync(cachedPath, 0o755)
+    core.addPath(cachedPath)
+    core.info(`Downloaded version ${version}`)
+  }
+
+  // Run `chalk login --client-id <client id> --client-secret <client secret> --api-host <api host> --environment`
+  // in a subprocess
+
+  const clientId = core.getInput('client-id')
+  const clientSecret = core.getInput('client-secret')
+  const apiHost = core.getInput('api-host') || 'https://api.prod.chalk.ai'
+  const environment = core.getInput('environment')
+
+  core.info(`Executing 'chalk login'`)
+  if (!clientId || !clientSecret) {
+    core.info(`${TOOL_NAME} is installed; skipping authentication because client-id or client-secret is not provided`)
+    return;
+  }
+  await execAsync(`${TOOL_NAME} login --client-id="${clientId}" --client-secret="${clientSecret}" --api-host="${apiHost}" --environment="${environment}" --no-prompt`)
+  core.info(`Executed 'chalk login'`)
+
+  core.info(`${TOOL_NAME} is installed`)
+}
+
+async function getLatestVersion(os: string, arch: string) {
+  core.info(`Checking latest version...`)
+  const versionResponse = await client.get(`https://api.prod.chalk.ai/v2/install/versions/${os}/${arch}`)
+  const body = await versionResponse.readBody()
+  return JSON.parse(body)['latest_version']
+}
+
+run().catch((error) => {
+  if (error instanceof Error) {
+    core.setFailed(error.message)
+  } else {
+    core.setFailed(`${error}`)
+  }
+})
